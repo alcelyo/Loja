de67516cea147230df08c295b35250438a77463b
Alcelyo Roberto <alcelyo@gmail.com>
2011-10-18 22:15:41 -0300

    Primeiro comite

---
+++ app/models/produto.rb
@@ -0,0 +1,4 @@
+class Produto < ActiveRecord::Base
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loja::Application.config.secret_token = '7085a51fd9ca6e2298d2e66d5dafc6eb8a3bff5fa3d97abfe44044031b835d0d5681f475adb3cc036fc7c3574fdd1ba32b69057efed5cf565906ab4c574afb8d'
